086f2971869fa6977825ff7b069005038e900199
Jawadrasheed01 <jawadrasheed01@gmail.com>
2024-05-27 16:27:30 +0500

    Add New Project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import First from "./Components/First";
+import Navbar from "./Components/Navbar";
+import Model from "./Model";
+import Practice from "./Practice";
+import Practice2 from "./Model2";
+import Model2 from "./Model2";
+
+
+export default function App() {
+  return (
+   <>
+  
+   
+   {/* <Navbar/> */}
+   
+   {/* <First/> */}
+
+
+   {/* <Practice/> */}
+   <Model/>
+
+   {/* <Practice2/> */}
+   {/* <Model2/> */}
+   </>
+  )
+}
